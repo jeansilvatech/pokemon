aa5923e1ef46ecef414aa9f1679af4b28add3989
Jean Silva <jeanpsilva10@gmail.com>
2022-10-06 18:14:41 -0300

    Atualizando background de acordo com o tipo do pokemon

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const form = document.querySelector('form');
 const span = document.querySelector('span');
 const card = document.querySelector('.card');
 const input = document.querySelector('input');
+const header = document.querySelector('header');
 const url = 'https://pokeapi.co/api/v2/pokemon-form/'
 let id = 0
 
@@ -29,57 +30,75 @@ function bgCard(typePokemon){
     switch(typePokemon){
         case 'grass':
             card.style.backgroundColor = '#3CB371'
+            header.style.backgroundColor = '#7FFFD4'
         break;
         case 'fire':
             card.style.backgroundColor = '#FFA500'
+            header.style.backgroundColor = '#FFA07A'
         break;
         case 'water':
             card.style.backgroundColor = '#1E90FF'
+            header.style.backgroundColor = '#87CEEB'
         break;
         case 'bug':
             card.style.backgroundColor = '#729f3f'
+            header.style.backgroundColor = '#90EE90'
         break;
         case 'normal':
             card.style.backgroundColor = '#808080'
+            header.style.backgroundColor = '#DCDCDC'
         break;
         case 'poison':
             card.style.backgroundColor = '#9370DB'
+            header.style.backgroundColor = '#D8BFD8'
         break;
         case 'electric':
             card.style.backgroundColor = '#FFD700'
+            card.style.backgroundColor = '#EEE8AA'
         break;
         case 'ground':
             card.style.backgroundColor = '#F0E68C'
+            card.style.backgroundColor = '#F5F5DC'
         break;
         case 'fairy':
             card.style.backgroundColor = '#EE82EE'
+            card.style.backgroundColor = '#DDA0DD'
         break;
         case 'fighting':
             card.style.backgroundColor = '#d56723'
+            card.style.backgroundColor = '#F4A460'
         break;
         case 'psychic':
             card.style.backgroundColor = '#FF69B4'
+            card.style.backgroundColor = '#FFB6C1'
         break;
         case 'rock':
             card.style.backgroundColor = '#a38c21'
+            card.style.backgroundColor = '#BDB76B'
         break;
         case 'ghost':
             card.style.backgroundColor = '#7b62a3'
+            card.style.backgroundColor = '#E6E6FA'
         break;
         case 'ice':
             card.style.backgroundColor = '#00BFFF'
+            card.style.backgroundColor = '#87CEFA'
         break;
         case 'steel':
             card.style.backgroundColor = '#9eb7b8'
+            card.style.backgroundColor = '#B0C4DE'
         break;
         case 'dragon':
-            card.style.backgroundColor = '#f16e57'
+            card.style.backgroundColor = '#DC143C'
+            card.style.backgroundColor = '#FA8072'
         break;
         case 'dark':
             card.style.backgroundColor = '#707070'
+            card.style.backgroundColor = '#9370DB'
         break;
         case 'flying':
             card.style.backgroundColor = '#4169E1'
+            card.style.backgroundColor = '#6495ED'
         break;
         default:
         card.style.backgroundColor = 'rgba(255, 255, 255, 0.1)' 
