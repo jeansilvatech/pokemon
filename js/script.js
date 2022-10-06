db70ef04c0e0b5dad10e4f62ff007629b2294bb8
Jean Silva <jeanpsilva10@gmail.com>
2022-10-06 16:30:04 -0300

    Adição de tipo e mudança de cor do bg de acordo com o tipo

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ const image = document.querySelector('.image')
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
 const form = document.querySelector('form');
+const span = document.querySelector('span');
+const card = document.querySelector('.card');
 const input = document.querySelector('input');
-const url = 'https://pokeapi.co/api/v2/pokemon/'
+const url = 'https://pokeapi.co/api/v2/pokemon-form/'
 let id = 0
 
 const pokeApi = async(id)=>{
@@ -23,11 +25,80 @@ function getImage(id){
         image.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`
     }
 }
+function bgCard(typePokemon){
+    switch(typePokemon){
+        case 'grass':
+            card.style.backgroundColor = '#3CB371'
+        break;
+        case 'fire':
+            card.style.backgroundColor = '#FFA500'
+        break;
+        case 'water':
+            card.style.backgroundColor = '#1E90FF'
+        break;
+        case 'bug':
+            card.style.backgroundColor = '#729f3f'
+        break;
+        case 'normal':
+            card.style.backgroundColor = '#808080'
+        break;
+        case 'poison':
+            card.style.backgroundColor = '#9370DB'
+        break;
+        case 'electric':
+            card.style.backgroundColor = '#FFD700'
+        break;
+        case 'ground':
+            card.style.backgroundColor = '#F0E68C'
+        break;
+        case 'fairy':
+            card.style.backgroundColor = '#EE82EE'
+        break;
+        case 'fighting':
+            card.style.backgroundColor = '#d56723'
+        break;
+        case 'psychic':
+            card.style.backgroundColor = '#FF69B4'
+        break;
+        case 'rock':
+            card.style.backgroundColor = '#a38c21'
+        break;
+        case 'ghost':
+            card.style.backgroundColor = '#7b62a3'
+        break;
+        case 'ice':
+            card.style.backgroundColor = '#00BFFF'
+        break;
+        case 'steel':
+            card.style.backgroundColor = '#9eb7b8'
+        break;
+        case 'dragon':
+            card.style.backgroundColor = '#f16e57'
+        break;
+        case 'dark':
+            card.style.backgroundColor = '#707070'
+        break;
+        case 'flying':
+            card.style.backgroundColor = '#4169E1'
+        break;
+        default:
+        card.style.backgroundColor = 'rgba(255, 255, 255, 0.1)' 
+    }
+}
 const renderPokemon = async(id)=>{
     const data = await pokeApi(id);
+    const typePokemon = await data.types[0].type.name
+    const typeTwo = await data.types
     namePokemon.innerHTML = data.name;
     getImage(data.id);
     idPokemon.innerHTML = data.id;
+    if(typeTwo.length ===2){
+        const typePokemon2 = await data.types[1].type.name
+        span.innerHTML = `${typePokemon.toUpperCase()} | ${typePokemon2.toUpperCase()}`
+    }else{
+        span.innerHTML = `${typePokemon.toUpperCase()}`
+    }
+    bgCard(typePokemon); 
 }
 
 form.addEventListener('submit', (event)=>{
@@ -36,11 +107,7 @@ form.addEventListener('submit', (event)=>{
     console.log(input.value)
     input.value = ''
 })
-
-next.addEventListener('click', ()=>{
-            id += 1
-            renderPokemon(id)
-        })      
+      
 
 prev.addEventListener('click', ()=>{
             id -= 1
@@ -49,3 +116,10 @@ prev.addEventListener('click', ()=>{
             }
             renderPokemon(id)
         })  
+next.addEventListener('click', ()=>{
+            id += 1
+            if(id>905){
+                id=905
+            }
+            renderPokemon(id)
+        })
