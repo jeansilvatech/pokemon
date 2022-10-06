d951a9779d632bf46bc73f7d7f34da8c5910a0c2
Jean Silva <jeanpsilva10@gmail.com>
2022-10-06 11:09:02 -0300

    Adicionando busca por id e nome

--- js/script.js
+++ js/script.js
@@ -3,10 +3,17 @@ const idPokemon = document.querySelector('h2')
 const image = document.querySelector('.image')
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
-
+const form = document.querySelector('form');
+const input = document.querySelector('input');
 const url = 'https://pokeapi.co/api/v2/pokemon/'
 let id = 0
-function getImage(){
+
+const pokeApi = async(id)=>{
+    const pokeResponse = await  fetch(`${url}${id}`);
+    const data = await pokeResponse.json();
+    return data;
+}
+function getImage(id){
     if(id<10){
         image.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/00${id}.png`
     }else if(id>=10 && id<100){
@@ -15,31 +22,26 @@ function getImage(){
         image.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`
     }
 }
-    next.addEventListener('click', ()=>{
-        
-            id += 1
-            console.log(id)
-           fetch(`${url}${id}`)
-            .then((response)=>{
-                return response.json();
-            }).then( async(data)=>{
-                namePokemon.innerHTML = await data.name;
-                getImage();
-                idPokemon.innerHTML = id;
-            }) 
+const renderPokemon = async(id)=>{
+    const data = await pokeApi(id);
+    namePokemon.innerHTML = data.name;
+    getImage(data.id);
+    idPokemon.innerHTML = data.id;
+}
 
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    renderPokemon(input.value.toLocaleLowerCase());
+    console.log(input.value)
+    input.value = ''
+})
+    next.addEventListener('click', ()=>{
+            id += 1
+            renderPokemon(id)
         })      
 
         prev.addEventListener('click', ()=>{
             id -= 1
-           fetch(`${url}${id}`)
-            .then((response)=>{
-                return response.json();
-            }).then((data)=>{
-                namePokemon.innerHTML = data.name;
-                getImage();
-                idPokemon.innerHTML = id;
-            }) 
-
+            renderPokemon(id)
         })  
        
