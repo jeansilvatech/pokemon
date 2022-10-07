185934462be0aa1f598d5e5046d31cdcd6b920f0
Jean Silva <jeanpsilva10@gmail.com>
2022-10-07 10:48:14 -0300

    background linear gradient

--- js/script.js
+++ js/script.js
@@ -30,78 +30,78 @@ function bgCard(typePokemon){
     switch(typePokemon){
         case 'grass':
             card.style.backgroundColor = '#3CB371'
-            header.style.backgroundColor = '#7FFFD4'
+            header.style.background = 'linear-gradient(to bottom, #11998e, #38ef7d)'
         break;
         case 'fire':
             card.style.backgroundColor = '#FFA500'
-            header.style.backgroundColor = '#FFA07A'
+            header.style.background = 'linear-gradient(to bottom, #f12711, #f5af19)'
         break;
         case 'water':
             card.style.backgroundColor = '#1E90FF'
-            header.style.backgroundColor = '#87CEEB'
+            header.style.background = 'linear-gradient(to bottom, #2193b0, #6dd5ed)'
         break;
         case 'bug':
             card.style.backgroundColor = '#729f3f'
-            header.style.backgroundColor = '#90EE90'
+            header.style.background = 'linear-gradient(to bottom, #56ab2f, #a8e063)'
         break;
         case 'normal':
             card.style.backgroundColor = '#808080'
-            header.style.backgroundColor = '#DCDCDC'
+            header.style.background = 'linear-gradient(to bottom, #304352, #d7d2cc)'
         break;
         case 'poison':
             card.style.backgroundColor = '#9370DB'
-            header.style.backgroundColor = '#D8BFD8'
+            header.style.background = 'linear-gradient(to bottom, #834d9b, #d04ed6)'
         break;
         case 'electric':
             card.style.backgroundColor = '#FFD700'
-            header.style.backgroundColor = '#EEE8AA'
+            header.style.background = 'linear-gradient(to bottom, #ffe000, #1c1c1c)'
         break;
         case 'ground':
             card.style.backgroundColor = '#F0E68C'
-            header.style.backgroundColor = '#F5F5DC'
+            header.style.background = 'linear-gradient(to bottom, #cac531, #f3f9a7)'
         break;
         case 'fairy':
             card.style.backgroundColor = '#EE82EE'
-            header.style.backgroundColor = '#DDA0DD'
+            header.style.background = 'linear-gradient(to bottom, #f4c4f3, #fc67fa)'
         break;
         case 'fighting':
             card.style.backgroundColor = '#d56723'
-            header.style.backgroundColor = '#F4A460'
+            header.style.background = 'linear-gradient(to bottom, #ff5f6d, #ffc371)'
         break;
         case 'psychic':
             card.style.backgroundColor = '#FF69B4'
-            header.style.backgroundColor = '#FFB6C1'
+            header.style.background = 'linear-gradient(to bottom, #b24592, #f15f79)'
         break;
         case 'rock':
             card.style.backgroundColor = '#a38c21'
-            header.style.backgroundColor = '#BDB76B'
+            header.style.background = 'linear-gradient(to bottom, #414d0b, #727a17)'
         break;
         case 'ghost':
             card.style.backgroundColor = '#7b62a3'
-            header.style.backgroundColor = '#E6E6FA'
+            header.style.background = 'linear-gradient(to bottom, #9d50bb, #6e48aa)'
         break;
         case 'ice':
             card.style.backgroundColor = '#00BFFF'
-            header.style.backgroundColor = '#87CEFA'
+            header.style.background = 'linear-gradient(to bottom, #c0c0aa, #1cefff)'
         break;
         case 'steel':
             card.style.backgroundColor = '#9eb7b8'
-            header.style.backgroundColor = '#B0C4DE'
+            header.style.background = 'linear-gradient(to bottom, #e6dada, #274046)'
         break;
         case 'dragon':
             card.style.backgroundColor = '#DC143C'
-            header.style.backgroundColor = '#FA8072'
+            header.style.background = 'linear-gradient(to bottom, #ed213a, #93291e)'
         break;
         case 'dark':
             card.style.backgroundColor = '#707070'
-            header.style.backgroundColor = '#9370DB'
+            header.style.background = 'linear-gradient(to bottom, #232526, #414345)'
         break;
         case 'flying':
             card.style.backgroundColor = '#4169E1'
-            header.style.backgroundColor = '#6495ED'
+            header.style.background = 'linear-gradient(to bottom, #0575e6, #021b79)'
         break;
         default:
-        card.style.backgroundColor = 'rgba(255, 255, 255, 0.1)' 
+        card.style.background = 'rgba(255, 255, 255, 0.1)' 
     }
 }
 const renderPokemon = async(id)=>{
